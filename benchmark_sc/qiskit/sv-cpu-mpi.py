0a25f67fd749488edb99c6d6ac465be71f02722c
heheda <zhangch99@outlook.com>
2022-02-26 19:34:10 +0800

    scripts for qiskit cpu

---
+++ benchmark_sc/qiskit/sv-cpu-mpi.py
@@ -0,0 +1,40 @@
+import os
+import qiskit
+from qiskit import QuantumCircuit
+# from qiskit.providers.aer import AerSimulator
+from qiskit import Aer
+import time
+import importlib
+import ctypes
+_mpi = ctypes.CDLL('libmpi.so')
+
+def run(filename):
+    file_path = os.path.join(f"/home/heheda/QCSimulator/tests/input-extend/{filename}.qasm")
+    simulator = Aer.get_backend('aer_simulator_statevector')
+    options = {
+        "method": "statevector",
+        "device": "CPU",
+        "precision": "double",
+        "blocking_enable": True
+    }
+    simulator.set_options(**options)
+    circ = QuantumCircuit.from_qasm_file(file_path)
+    circ.save_statevector()
+    start = time.time()
+    result = qiskit.execute(circ, backend=simulator).result()
+    end = time.time()
+    print(filename, result.time_taken, end - start)
+
+if __name__ == "__main__":
+    cases = [
+        "basis_change_24",
+        "bv_24",
+        "efficient_su2_25",
+        "hidden_shift_27",
+        "qft_26",
+        "supremacy_25"
+    ]
+
+    for case in cases:
+        run(case)
+
