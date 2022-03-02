dda6161bd06e33d54d4846c8cd1786a9ebe6a360
heheda <zhangch99@outlook.com>
2022-03-02 10:22:17 +0800

    update scripts for cpu

--- benchmark_sc/qiskit/sv-gpu-single.py
+++ benchmark_sc/qiskit/sv-gpu-single.py
@@ -6,12 +6,10 @@ import time
 
 def run(filename):
     file_path = os.path.join(f"/home/heheda/QCSimulator/tests/input-extend/{filename}.qasm")
-    # file_path = os.path.join(f"/home/heheda/QCSimulator/tests/{filename}.qasm")
     simulator = Aer.get_backend('aer_simulator_statevector')
-    # print(simulator.options)
     options = {
         "method": "statevector",
-        "device": "CPU",
+        "device": "GPU",
         "precision": "double",
     }
     simulator.set_options(**options)
@@ -21,15 +19,13 @@ def run(filename):
     result = qiskit.execute(circ, backend=simulator).result()
     end = time.time()
     print(filename, result.time_taken, end - start, flush=True)
-    # print(result)
 
 if __name__ == "__main__":
     cases = [
+        "basis_change_29",
         "bv_27",
         "efficient_su2_28",
         "hidden_shift_27",
-        "iqp_25",
-        "qaoa_26",
         "qft_29",
         "supremacy_28"
     ]
