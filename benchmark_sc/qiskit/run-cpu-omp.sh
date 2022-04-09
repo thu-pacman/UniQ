ecf87cd1840e96deef93afb28f21d537fcdfa3f3
heheda <zhangch99@outlook.com>
2022-04-09 16:30:13 +0800

    remove previous benchmark script

--- benchmark_sc/qiskit/run-cpu-omp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-OMP_NUM_THREADS=1 python sv-cpu-omp.py
-OMP_NUM_THREADS=2 python sv-cpu-omp.py
-OMP_NUM_THREADS=4 python sv-cpu-omp.py
-OMP_NUM_THREADS=8 python sv-cpu-omp.py
-OMP_NUM_THREADS=16 python sv-cpu-omp.py
-OMP_NUM_THREADS=32 python sv-cpu-omp.py
-OMP_NUM_THREADS=64 python sv-cpu-omp.py
