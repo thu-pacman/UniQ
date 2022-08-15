fe88f4c4833b2674f7ed517f7dbf91e93e18ccc6
heheda <zhangch99@outlook.com>
2022-08-15 13:58:35 +0800

    add readme

--- scripts/gen_stdout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source init.sh -DBACKEND=1 -DSHOW_SUMMARY=off
-for test in ${tests[*]}; do
-    echo $test
-    ./main ../tests/input/$test.qasm > ../tests/output/$test.log
-done
--- src/logger.cpp
+++
@@ -1,3 +0,0 @@
-#include<logger.h>
-
-Logger* Logger::instance = NULL;
